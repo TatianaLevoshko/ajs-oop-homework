4ebf05c578bd06b18b8062d828a4432f6b67f3f4
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-07-07 12:32:38 +0300

    lf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,29 @@
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'airbnb-base'],
   env: {
     browser: true,
-    node: true,
-    es2023: true,
+    es2021: true,
     jest: true,
+    node: true,
   },
-  extends: 'airbnb-base',
-  rules: {},
+  plugins: ['prettier'],
+  rules: {
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'linebreak-style': ['error', 'windows'],
+    'no-tabs': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'always',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+      },
+    ],
+    quotes: ['error', 'single'],
+  },
 };
